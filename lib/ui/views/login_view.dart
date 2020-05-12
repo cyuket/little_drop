71438979c2d2a106c165dc27c244fcff34d963b0
cyuket <cyriluket12@gmail.com>
2020-05-13 00:00:56 +0100

    Authentication with firebase
    Login Auth completed

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -8,6 +8,8 @@ import 'package:little_drops/ui/widgets/input_field.dart';
 import 'package:little_drops/ui/widgets/busy_button.dart';
 
 import 'package:little_drops/constants/route_names.dart';
+import 'package:little_drops/viewModel/login_view_model.dart';
+import 'package:provider_architecture/_viewmodel_provider.dart';
 
 class LoginView extends StatelessWidget {
   final emailController = TextEditingController();
@@ -16,85 +18,97 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 414, height: 896, allowFontScaling: true);
-    return Scaffold(
-      backgroundColor: AppColors().background,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: AppColors().background,
-        automaticallyImplyLeading: false,
-        title: Text(
-          'Login',
-          style: GoogleFonts.lato(
-              textStyle: TextStyle(fontSize: 20, color: AppColors().textColor)),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          width: screenWidth(context),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(child: Image.asset(AppAsset().illustration4)),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Text(
-                  'Kindly fill in your details',
-                  style: GoogleFonts.lato(
-                      textStyle: TextStyle(
-                          fontSize: 15, color: AppColors().textColor)),
-                ),
+    return ViewModelProvider<LoginViewModel>.withConsumer(
+        viewModelBuilder: () => LoginViewModel(),
+        builder: (context, model, child) {
+          return Scaffold(
+            backgroundColor: AppColors().background,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: AppColors().background,
+              automaticallyImplyLeading: false,
+              title: Text(
+                'Login',
+                style: GoogleFonts.lato(
+                    textStyle:
+                        TextStyle(fontSize: 20, color: AppColors().textColor)),
               ),
-              InputField(
-                smallVersion: false,
-                controller: emailController,
-                label: "Email",
-                placeholder: "youremail@email.com",
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              InputField(
-                smallVersion: false,
-                controller: passwordController,
-                label: "Password",
-                placeholder: "Password",
-                password: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              BusyButton(
-                  title: 'Login',
-                  onPressed: () => Navigator.pushNamed(context, HomeViewRoute)),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Row(
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: screenWidth(context),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text('Not a new member? ',
+                    Center(child: Image.asset(AppAsset().illustration4)),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Text(
+                        'Kindly fill in your details',
                         style: GoogleFonts.lato(
-                          textStyle: TextStyle(
-                              fontSize: 12, color: AppColors().textColor),
-                        )),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushReplacementNamed(
-                            context, SignUpViewRoute);
-                      },
-                      child: Text('Signup',
-                          style: GoogleFonts.lato(
                             textStyle: TextStyle(
-                                fontSize: 12, color: AppColors().primaryColor),
-                          )),
-                    )
+                                fontSize: 15, color: AppColors().textColor)),
+                      ),
+                    ),
+                    InputField(
+                      smallVersion: false,
+                      controller: emailController,
+                      label: "Email",
+                      placeholder: "youremail@email.com",
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputField(
+                      smallVersion: false,
+                      controller: passwordController,
+                      label: "Password",
+                      placeholder: "Password",
+                      password: true,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    BusyButton(
+                      title: 'Login',
+                      busy: model.busy,
+                      onPressed: () {
+                        model.login(
+                            email: emailController.text,
+                            password: passwordController.text);
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text('Not a new member? ',
+                              style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                    fontSize: 12, color: AppColors().textColor),
+                              )),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(
+                                  context, SignUpViewRoute);
+                            },
+                            child: Text('Signup',
+                                style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                      fontSize: 12,
+                                      color: AppColors().primaryColor),
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
+        });
   }
 }
